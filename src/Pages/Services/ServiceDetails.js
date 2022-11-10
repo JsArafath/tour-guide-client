05f9a099afe7f0604d312ae9da57c6e839d4b021
JsArafath <arafatrahman077777@gmail.com>
2022-11-10 23:54:11 +0600

    login

--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { AuthProvider } from "../../Context/AuthProvider";
-// import axios from "axios";
+import axios from "axios";
 import toast from "react-hot-toast";
 
 
