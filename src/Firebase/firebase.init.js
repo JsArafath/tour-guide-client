ee2ce7b4b126e778b72910a5f6b406794f72853c
Md Mohaiminul Islam <99108994+JsArafath@users.noreply.github.com>
2022-11-11 21:52:45 +0600

    Update firebase.init.js

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB70CeZFTMt-2Zxg1vl4XcenZOJ0m5YdXw",
-  authDomain: "tour-guide-client.firebaseapp.com",
-  projectId: "tour-guide-client",
-  storageBucket: "tour-guide-client.appspot.com",
-  messagingSenderId: "1029939707123",
-  appId: "1:1029939707123:web:d4cf9e01519d760a5dcb86",
-  measurementId: "G-Y92QHQDDBP"
+  apiKey: process.env.REACT_APP_apiKey,             
+  authDomain: process.env.REACT_APP_authDomain,         
+  projectId: process.env.REACT_APP_projectId,         
+  storageBucket: process.env.REACT_APP_storageBucket,         
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,         
+  appId: process.env.REACT_APP_appId,  
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
